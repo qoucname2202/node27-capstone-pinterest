442044a0ab1e0f50f959586cd8558700df4b45c4
Nam Duong <duongquocnam224400@gmail.com>
2023-03-11 21:53:17 +0700

    Fix swagger and update signin-signup-testToken swagger

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ let bodyParser = require('body-parser');
 const config = require('./config');
 const cookieParser = require('cookie-parser');
 let rootRoute = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 app.use(express.json());
 
@@ -18,8 +20,19 @@ app.use(cookieParser());
 app.use(morgan('common'));
 app.use(express.static('.'));
 
+const options = {
+  definition: {
+    info: {
+      title: 'API',
+      version: 'v1',
+      description: '/swagger/v1/swagger.json',
+    },
+  },
+  apis: ['src/swagger/index.js'],
+};
+const specs = swaggerJsDoc(options);
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(specs));
 app.use('/api', rootRoute);
-
 const port = config.port ?? 3001;
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
---
+++ src/swagger/index.js
@@ -0,0 +1,57 @@
+/**
+ * @swagger
+ * /api/v1/auth/signup:
+ *  post:
+ *      tags: [Auth]
+ *      parameters:
+ *      - in: body
+ *        name: model
+ *        schema:
+ *           properties:
+ *             email:
+ *               type: string
+ *             password:
+ *               type: string
+ *             name:
+ *               type: string
+ *             age:
+ *               type: number
+ *      responses:
+ *          200:
+ *              description: Success
+ */
+
+/**
+ * @swagger
+ * /api/v1/auth/signin:
+ *  post:
+ *      tags: [Auth]
+ *      parameters:
+ *      - in: body
+ *        name: model
+ *        schema:
+ *           properties:
+ *             email:
+ *               type: string
+ *             password:
+ *               type: string
+ *      responses:
+ *          200:
+ *              description: success
+ */
+
+/**
+ * @swagger
+ * /api/v1/users/test-token:
+ *  post:
+ *      tags: [User]
+ *      parameters:
+ *       - in: header
+ *         description: Please enter Bearer [token]
+ *         name: Authorization
+ *         required: true
+ *         type: string
+ *      responses:
+ *          200:
+ *              description: success
+ */
