3c2937374e42e3d6c815f2846979b31294d58a86
Nam Duong <duongquocnam224400@gmail.com>
2023-03-11 12:05:18 +0700

    Done refresh token pinterest

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,8 +1,66 @@
 const responseMess = require('../config/response');
 const moment = require('moment');
 const config = require('../config');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const { generateToken, generateRefreshToken, checkRefreshToken } = require('../middlewares/jwt');
 
 const userControllers = {
+  refreshToken: async (req, res) => {
+    try {
+      const cookie = req.cookies;
+      if (!cookie || !cookie.refreshToken) {
+        return responseMess.badRequest(res, '', 'No refresh token in cookies!');
+      } else {
+        const userSchema = checkRefreshToken(cookie.refreshToken);
+        let { user_id, email, name } = userSchema;
+        let result = await prisma.user.findUnique({
+          where: {
+            email: email,
+          },
+        });
+        if (result) {
+          const newAccessToken = generateToken(result);
+          const newRefreshToken = generateRefreshToken(result);
+          // Save refresh token to database
+          await prisma.user.update({
+            where: {
+              email: email,
+            },
+            data: {
+              refresh_token: newRefreshToken,
+            },
+          });
+          res.cookie(config.refreshTokenName, newRefreshToken, {
+            httpOnly: true,
+            secure: false,
+            path: '/',
+            sameSite: 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+          });
+          return responseMess.success(res, { user_id, name, email, newAccessToken }, 'Refresh token successfully!');
+        } else {
+          return responseMess.badRequest(res, '', 'Refresh token not matched!');
+        }
+      }
+    } catch (err) {
+      responseMess.error(res, 'Internal Server Error');
+    }
+  },
+  forgotPassword: async (req, res) => {
+    try {
+      responseMess.success(res, 'Forgot password', 'Successfully!');
+    } catch (err) {
+      responseMess.error(res, 'Internal Server Error');
+    }
+  },
+  resetPassword: async (req, res) => {
+    try {
+      responseMess.success(res, 'Reset Password', 'Successfully!');
+    } catch (err) {
+      responseMess.error(res, 'Internal Server Error');
+    }
+  },
   getAllUser: async (req, res) => {
     try {
       responseMess.success(res, 'Get all user', 'Successfully!');
@@ -24,27 +82,7 @@ const userControllers = {
       responseMess.error(res, 'Internal Server Error');
     }
   },
-  refreshToken: async (req, res) => {
-    try {
-      responseMess.success(res, 'Refresh token', 'Successfully!');
-    } catch (err) {
-      responseMess.error(res, 'Internal Server Error');
-    }
-  },
-  forgotPassword: async (req, res) => {
-    try {
-      responseMess.success(res, 'Forgot password', 'Successfully!');
-    } catch (err) {
-      responseMess.error(res, 'Internal Server Error');
-    }
-  },
-  resetPassword: async (req, res) => {
-    try {
-      responseMess.success(res, 'Reset Password', 'Successfully!');
-    } catch (err) {
-      responseMess.error(res, 'Internal Server Error');
-    }
-  },
+
   getImagesUserCreate: async (req, res) => {
     try {
       responseMess.success(res, 'Get images user create', 'Successfully!');
